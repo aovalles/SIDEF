17bb817c267a5e6189f4703122dc370f96a568f3
aovalles <ovalles.alex@gmail.com>
2015-07-24 08:38:05 -0400

    sexto commit

--- Sidefi/wsgi.py
+++ Sidefi/wsgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SitioWeb.settings")
 
 #application = get_wsgi_application()
 
-from dj_static import Cling(get_wsgi_application())
+
+
+application = Cling(get_wsgi_application())
 
