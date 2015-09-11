4c86c70e39622f6f3edd6b17707d9d244b14fc26
aovalles <ovalles.alex@gmail.com>
2015-09-10 22:33:57 -0400

    commit

--- SitioWeb/urls.py
+++ SitioWeb/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     # url(r'^$', 'SitioWeb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    (r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset',
+    url(r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset',
      {'post_reset_redirect' : '/accounts/password/reset/done/'}),
-    (r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm',
-     {'post_reset_redirect' : '/accounts/password/done/'}),
-    (r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
+    url(r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+    url(r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm',
+    url{'post_reset_redirect' : '/accounts/password/done/'}),
+    url(r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
 
 
     url(r'^admin/', include(admin.site.urls)),
