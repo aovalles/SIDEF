96431ceeeba86dfa1c2eec996b6d009f29d650d2
aovalles <ovalles.alex@gmail.com>
2015-09-14 21:00:36 -0400

    commit

--- SitioWeb/urls.py
+++ SitioWeb/urls.py
@@ -12,7 +12,10 @@ urlpatterns = [
     # url(r'^$', 'SitioWeb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^accounts/login', 'Sidefi.views.signin', name="signin"), 
+
     url(r'^accounts/', include('django.contrib.auth.urls')),
+
 
     # url(r'^accounts/password/reset/$',
     #      'django.contrib.auth.views.password_reset',
